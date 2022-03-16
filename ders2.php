46abca86ac3065db70fb52aa4f1f576763981eaf
Furkan GÜLER <44furkanguler07@gmail.com>
2022-03-16 15:44:37 +0300

    https://github.com/FurkanGuler44/Web.git

--- ders2.php
+++ ders2.php
@@ -1,30 +1,93 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ders-2</title>
 </head>
+
 <body>
     <?php
-    
-    echo "Tırnak İşaretleri Arasındaki Farklar</h4>";
-    echo "<br>";
-    echo "Adnan Menderes Üniversitesi";
-    echo "<br>";
-    $uni="Adnan Menderes Üniversitesi";
-    echo "<br>";
 
-    echo "1-Kazandığınız Üniversite:" . '$uni';/* Değişkenin ismini yazdırır.*/
+    echo "<h4>Tırnak İşaretleri Arasındaki Farklar</h4>";
+
+    $uni = "Adnan Menderes Üniversitesi";
+    echo "Değişken İçeriği: $uni";
     echo "<br>";
-    echo "2-Kazandığınız Üniversite:" . "$uni";/* Değişkenin içeriğini yazdırır.*/
+    /* Değişkenin ismini yazdırır. */
+    echo "1-Kazandığınız Üniversite: " . '$uni';
     echo "<br>";
-    echo "3-Kazandığınız Üniversite:" . $uni;/* Değişkenin içeriğini yazdırır.*/
+    /* Değişkenin içeriğini yazdırır. */
+    echo "2-Kazandığınız Üniversite: " . "$uni";
     echo "<br>";
-    echo "4-Kazandığınız Üniversite: $uni";/* Değişkenin içeriğini yazdırır.*/
+    /* Değişkenin içeriğini yazdırır. */
+    echo "3-Kazandığınız Üniversite: " . $uni;
+    echo "<br>";
+    /* Değişkenin içeriğini yazdırır. */
+    echo "4-Kazandığınız Üniversite: $uni";
+
+    echo "<hr><h4>Temel Matematiksel İşlemler</h4>";
+    
+    echo "<h5>Toplama İşlemi</h5>";
+
+    $sayi1 = 10;
+    $sayi2 = 20;
+    echo '$sayi1 + $sayi2 = ' . ($sayi1 + $sayi2);
+    echo "<br>";
+    echo "$sayi1 + $sayi2 = " .  ($sayi1 + $sayi2);
+    echo "<br>";
+    $sonuc = $sayi1 + $sayi2;
+    echo "$sayi1 + $sayi2 = $sonuc";
+
+    echo "<h5>Çıkarma İşlemi</h5>";
+    $sayi1=10;
+    $sayi2=20;
+    echo '$sayi1 - $sayi2=' . ($sayi1 - $sayi2);
+    echo "<br>";
+    echo "$sayi1 - $sayi2=" . ($sayi1 - $sayi2);
+    echo "<br>";
+    $sonuc = $sayi1 - $sayi2;
+    echo "$sayi1 + $sayi2 = $sonuc";
+
+    echo "<h5>Çarpma İşlemi</h5>";
+    $sayi1=10;
+    $sayi2=20;
+    echo '$sayi1 * $sayi2=' . ($sayi1 * $sayi2);
+    echo "<br>";
+    echo "$sayi1 * $sayi2=" . ($sayi1 * $sayi2);
+    echo "<br>";
+    $sonuc = $sayi1 * $sayi2;
+    echo "$sayi1 * $sayi2 = $sonuc";
+
+    echo "<h5>Bölme İşlemi</h5>";
+    $sayi1=10;
+    $sayi2=20;
+    echo '$sayi1 / $sayi2=' . ($sayi1 / $sayi2);
+    echo "<br>";
+    echo "$sayi1 / $sayi2=" . ($sayi1 / $sayi2);
+    echo "<br>";
+    $sonuc = $sayi1 / $sayi2;
+    echo "$sayi1 / $sayi2 = $sonuc";
+
+    echo "<h5>Üst Alma İşlemi (pow)</h5>";
+    $x=3;
+    $y=2;
+    echo "$x<sup>$y</sup>=" . (pow($x,$y));
+    echo "<br> Değişken Tipi:" . gettype($x);
+
+    echo "<h5>Karekök Alma İşlemi (sqrt)</h5>";
+    $x=25;
+    $karekok=sqrt($x);
+    echo "$x'in karekökü $karekok'tir.";
     
     ?>
 
+
+<br><br><br><br><br><br><br><br><br><br><br><br>
+
+
 </body>
+
 </html>
