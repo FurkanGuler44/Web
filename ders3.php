42d30df4a0984ba47b135b0773f94ffbfe98adf1
Furkan GÜLER <44furkanguler07@gmail.com>
2022-03-23 15:22:02 +0300

    htttps://github.com/FurkanGuler44/Web.git

--- ders3.php
+++ ders3.php
@@ -12,20 +12,33 @@
         /* Uygulama Ödevi
         Kuyruk ve yığın veri yapılarının php üzerinde dizileri kullanılarak örneklendiriniz.*/
 
+        /* Kuyruk(Queue) Yapısı 
+        $dizi = array("Audi","Volkswagen","BMW","Mercedes-Benz");
+        array_push($dizi,"Porsche","Opel");
+        echo $dizi;*/
+
+        /* Yığın(Stack) Yapısı 
+        $kume = rand(10,100);
+        echo $kume;
+        */
+
+
+
         /* Rastgele üretilen sayı 
         50'den büyükse 
         Geçtiniz,Notunuz:XX
         50'den küçükse
         kaldınız,notunuz:XX
         uygulamasını gerçekleştiriniz.*/
-        $rastgele_sayi = rand(10,100);
+        
+         $rastgele_sayi = rand(10,100);
         if($rastgele_sayi >= 50) {
             echo "Geçtiniz - Notunuz:$rastgele_sayi";
         }
         else {
             echo "Kaldınız - Notunuz:$rastgele_sayi"; 
         }
-
+        
 
     ?>
 
